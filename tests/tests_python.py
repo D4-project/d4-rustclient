170b13e656d2188c683ac31ddc34513fd29988b7
Raphaël Vinot <raphael@vinot.info>
2021-08-20 11:00:20 +0200

    chg: Improve python tests

--- tests/tests_python.py
+++ tests/tests_python.py
@@ -2,28 +2,45 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import uuid
+import time
 
 from d4message import D4Message, from_bytes
 
 
 class SimpleTest(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.protocol_version = 1
+        cls.packet_type = 1
+        cls.sensor_uuid = uuid.uuid4()
+        cls.key = b'My Hmac key'
+        cls.message = b'blah'
+
     def test_hmac(self):
-        m = D4Message(1, 1, b'1111111111111111', b'1', b'blahh')
-        self.assertTrue(m.validate_hmac(b'1'))
+        m = D4Message(self.protocol_version, self.packet_type,
+                      self.sensor_uuid.bytes, self.key, self.message)
+        self.assertTrue(m.validate_hmac(self.key))
         self.assertFalse(m.validate_hmac(b'2'))
 
     def test_encode_decode(self):
-        m = D4Message(1, 1, b'1111111111111111', b'1', b'blahh')
+        m = D4Message(self.protocol_version, self.packet_type,
+                      self.sensor_uuid.bytes, self.key, self.message)
+        now = int(time.time())
+        diff = now - m.header.timestamp
+        self.assertTrue(diff <= 2, diff)
+
         encoded = bytes(m.to_bytes())
         decoded = from_bytes(encoded)
-        self.assertTrue(decoded.validate_hmac(b'1'))
+        self.assertTrue(decoded.validate_hmac(self.key))
         self.assertEqual(m.to_bytes(), decoded.to_bytes())
 
     def test_class_members(self):
-        m = D4Message(1, 1, b'1111111111111111', b'1', b'blahh')
-        self.assertEqual(m.header.protocol_version, 1)
-        self.assertEqual(m.header.packet_type, 1)
-        self.assertEqual(bytes(m.header.uuid), b'1111111111111111')
-        self.assertEqual(m.header.size, 5)
-        self.assertEqual(bytes(m.body), b'blahh')
+        m = D4Message(self.protocol_version, self.packet_type,
+                      self.sensor_uuid.bytes, self.key, self.message)
+        self.assertEqual(m.header.protocol_version, self.protocol_version)
+        self.assertEqual(m.header.packet_type, self.packet_type)
+        self.assertEqual(bytes(m.header.uuid), self.sensor_uuid.bytes)
+        self.assertEqual(m.header.size, len(self.message))
+        self.assertEqual(bytes(m.body), self.message)
